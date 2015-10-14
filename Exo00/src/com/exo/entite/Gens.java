6bd217d024f75ac0f340935ebb52772f26b93c31
chris <chris@chris-PC>
2015-10-14 16:43:04 +0200

    qsdf

---
+++ Exo00/src/com/exo/entite/Gens.java
@@ -0,0 +1,53 @@
+package com.exo.entite;
+
+import java.io.Serializable;
+
+public class Gens implements IPersonne, Serializable {
+
+	public Gens() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String getPrenom() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setPrenom(String prenom) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getAge() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setAge(int age) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getNom() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setNom(String unNom) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void inverser(IPersonne a) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
