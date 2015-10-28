fd130464e85c0931e5f10857c066b013182df52c
langchristine7 <lang.christine7@gmail.com>
2015-10-28 10:02:32 +0100

    renommage test

--- Exo11/src/fr/db/ExoDB.java
+++ Exo11/src/fr/db/ExoDB.java
@@ -6,7 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class TestDB {
+public class ExoDB {
 
 
 	public static void main(String[] args) {
